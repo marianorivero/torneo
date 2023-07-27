63244ff18cb37b8f19ddfc0626189c2e25fac05d
Mariano Rivero <marianorivero97@gmail.com>
2023-07-27 17:01:59 -0300

    -

--- funciones/posiblePrediccion.js
+++ funciones/posiblePrediccion.js
@@ -13,14 +13,12 @@ function prediccion(equipo1, equipo2) {
     let res2 = (primeraDiferenciaUltima) - (segundaDiferenciaUltima);
 
  
-    // console.log(res1+' '+res2);
-    if ( (res1 >=1 ) && (res2 >=1 ) ) {
-        //el primer equipo tiene la ventaja
-
+    
+    if ( (res1 >=1 ) && (res2 >=1 ) ) {//el primer equipo tiene la ventaja
         let miDiferencia = (res1) - (res2);
 
-        if ( (miDiferencia <= 10) && (miDiferencia >= 1)) {
-            //el primer equipo gana
+        if ( (miDiferencia <= 10) && (miDiferencia >= 1)) {//el primer equipo gana
+            
             porcentaje = `
             50% ${equipo1[0].nombre} <br>
             30%  EMPATE              <br>
@@ -28,15 +26,7 @@ function prediccion(equipo1, equipo2) {
             `;
             prediccion    = '<b>Afinidad:</b> <u>'+ equipo1[0].nombre+'</u>';
             hayPrediccion = true;
-
         }else{
-            // porcentaje = `
-            // 40%  EMPATE              <br>
-            // 40% ${equipo1[0].nombre} <br>
-            // 20% ${equipo2[0].nombre} <br>
-            // `;
-            // prediccion    = '<b>Afinidad:</b> Empate o '+ equipo1[0].nombre;
-            // hayPrediccion = true;
             porcentaje = ``;
             prediccion = '-';
             hayPrediccion = false;
@@ -44,13 +34,11 @@ function prediccion(equipo1, equipo2) {
 
 
     }else{
-        if ( (res1 <= -1) && (res2 <= -1)) {
-            //el segundo equipo tiene la ventaja
-
+        if ( (res1 <= -1) && (res2 <= -1)) {//el segundo equipo tiene la ventaja
             let miDiferencia = (res1) - (res2);
 
-            if ( (miDiferencia >= -10)  && (miDiferencia <= -1)) {
-                //el segundo equipo gana
+            if ( (miDiferencia >= -10)  && (miDiferencia <= -1)) {//el segundo equipo gana
+                
                 porcentaje = `
                 50% ${equipo2[0].nombre} <br>
                 30% EMPATE               <br>
@@ -59,13 +47,6 @@ function prediccion(equipo1, equipo2) {
                 prediccion    = '<b>Afinidad:</b> <u>'+ equipo2[0].nombre+'</u>';
                 hayPrediccion = true;
             }else{
-                // porcentaje = `
-                // 40%  EMPATE              <br>
-                // 40% ${equipo1[0].nombre} <br>
-                // 20% ${equipo2[0].nombre} <br>
-                // `;
-                // prediccion    = '<b>Afinidad:</b> Empate o '+ equipo2[0].nombre;
-                // hayPrediccion = true;
                 porcentaje = ``;
                 prediccion = '-';
                 hayPrediccion = false;
@@ -75,11 +56,9 @@ function prediccion(equipo1, equipo2) {
             prediccion = '-';
             hayPrediccion = false;
         }
-
     }
 
     const resultado = {
-
         partido:        equipo1[0].nombre+'  -  '+equipo2[0].nombre,
         hay_prediccion: hayPrediccion,
         prediccion:     prediccion,
